dd6397d07e9b8a8daf69afe220fb67d3f32427ac
ART0 <18333981+0Art0@users.noreply.github.com>
2023-12-15 21:53:30 +0000

    Tree display in tactic state

--- TreeRewritingGame/OrdinaryDisplay.lean
+++ TreeRewritingGame/OrdinaryDisplay.lean
@@ -1,38 +1,36 @@
 import TreeRewritingGame.DisplayTree
 import ProofWidgets 
 
-open Lean Meta Elab Tactic ProofWidgets 
+open Lean Meta Elab Server Tactic ProofWidgets 
 
-structure TreeDisplay where 
+structure TreeDisplay extends PanelWidgetProps where 
   tree : DisplayTree
-
-#mkrpcenc TreeDisplay
+deriving RpcEncodable
 
 @[widget_module]
 def OrdinaryTreeDisplay : Component TreeDisplay where
-  javascript := include_str ".." / "build" / "js" / "treeDisplay.js"
+  javascript := include_str ".." / "build" / "js" / "interactiveTreeDisplay.js"
 
-open scoped Jsx in
-def DisplayTree.draw (t : DisplayTree) : Html := <OrdinaryTreeDisplay tree={t}/>
+syntax (name := tree_display) "with_tree_display" tacticSeq : tactic
 
-@[expr_presenter]
-def DisplayTree.presenter : ExprPresenter where
-  userName := "Tree Display"
-  present e := withTransparency .instances do
-    let reduced ← reduceAll e
-    let t ← reduced.toDisplayTree
-    match t with 
-    | some t => return t.draw
-    | none => throwError "Cannot draw tree for expression {e}"
-
+@[tactic tree_display]
+def treeDisplay : Tactic
+  | stx@`(tactic| with_tree_display $tacs) => do
+    let tgt ← withTransparency .instances do 
+      reduceAll (← getMainTarget)
+    let t ← tgt.toDisplayTree 
+    savePanelWidgetInfo stx ``OrdinaryTreeDisplay do
+      return json% { tree : $(← rpcEncode t) }
+    evalTacticSeq tacs
+  | _ => throwUnsupportedSyntax
 
 example : True := by 
   with_panel_widgets[GoalTypePanel]
   sorry 
 
 example (P Q : Prop) : P ∧ Q → Q ∧ P := by 
-  with_panel_widgets[GoalTypePanel]
-  sorry
+  with_tree_display
+    sorry
 
 example (P : Prop) : P = True → P := by 
   with_panel_widgets[GoalTypePanel]
