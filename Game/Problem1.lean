577815448f60794903e224eb5749b1f3245e9067
Jonas Bayer <jonas.bayer999@gmail.com>
2023-12-20 15:01:27 +0100

    [Refactor] Removes alias extension
    * Remove alias extension
    * Clean up parts of `Expr.toDisplayTree`
    * Problems/Problem1.lean contains a demo setup of the new approach

--- Game/Problem1.lean
+++ Game/Problem1.lean
@@ -1,6 +1,6 @@
 import TreeRewritingGame.Problems.Problem1
 
-example (x y z : T0) : problem x y z := by 
+example (x y z : T₀) : problem x y z := by 
   with_panel_widgets[ProofWidgets.GoalTypePanel]
   rw [problem]
   sorry
--- TreeRewritingGame/Problems/Problem1.lean
+++ TreeRewritingGame/Problems/Problem1.lean
@@ -1,15 +1,23 @@
-import TreeRewritingGame.MaskedDisplay
+import TreeRewritingGame.TreeDisplay
 
-@[reducible] 
-def T0 := Nat
+def T₀ := Nat
+def A : T₀ → T₀ → T₀ := Nat.add
 
-def problem (a b c : Nat) : Prop := a * (b - c) = a*b - a*c
+def problem (x y z : T₀) : Prop := A x (A y z) = A (A y x) z
+
+def l0 : A x y = A y x := by 
+  rw [A] ; apply Nat.add_comm
+
+def l1 : A (A x y) z = A x (A y z) := by 
+  rw [A] ; apply Nat.add_assoc
 
 def library : Array Lean.Name := #[
-  `Nat.mul_sub_left_distrib
+  `l0, 
+  `l1
 ]
 
--- for testing purposes
-example (x y z : T0) : problem x y z := by 
+-- In this file for testing purposes
+example (x y z : T₀) : problem x y z := by 
   rw [problem]
-  rw [Nat.mul_sub_left_distrib]
+  with_panel_widgets[ProofWidgets.GoalTypePanel]
+  sorry
