6875bf0175dd62fb4390e924d6c5fc0e597dfa03
ART0 <18333981+0Art0@users.noreply.github.com>
2024-02-05 08:29:41 +0000

    Separate rendering from `locs` display

--- widget/src/interactiveTreeDisplay.tsx
+++ widget/src/interactiveTreeDisplay.tsx
@@ -8,7 +8,7 @@ export type DisplayTree =
 
 export type TreeNodeDatum = RawNodeDatum & { label?: CodeWithInfos }
 
-export type DisplayTreeProps = PanelWidgetProps & { tree : DisplayTree }
+export type DisplayTreeProps = PanelWidgetProps & { tree : DisplayTree, divStyle : object }
 
 function treeToData(tree: DisplayTree): TreeNodeDatum {
     const { label, children } = tree.node
@@ -35,7 +35,7 @@ function renderForeignObjectNode({ nodeDatum }: CustomNodeElementProps, _: Docum
   const nodeDatum_ = nodeDatum as TreeNodeDatum
   return (
     <g>
-      <rect x="-50" y="-10" width="100" height="20" fill="green" style={{ border: "black" }} />
+      <rect x="-50" y="-10" width="100" height="20" fill="aqua" style={{ border: "black" }} />
       <foreignObject {...foreignObjectProps} style={{ textAlign: "center" }}>
         {nodeDatum_.label && <InteractiveCode fmt={nodeDatum_.label} />}
       </foreignObject>
@@ -54,61 +54,49 @@ function centerTree (r : React.RefObject<HTMLDivElement>, t : any, setT : React.
     })
 }
 
-export function RenderDisplayTree({ pos, tree, r }: 
-    { pos: DocumentPosition, tree: DisplayTree, r : React.RefObject<HTMLDivElement> }): 
-    JSX.Element {
+export default function RenderDisplayTree(props : DisplayTreeProps): JSX.Element {
     const nodeSize = { x: 120, y: 40 }
     const foreignObjectProps = { width: 100, height: 30, y: -10, x: -50 }
     const [t, setT] = React.useState<any | null>(null)
+    const r = React.useRef<HTMLDivElement>(null)
     centerTree(r, t, setT)
     return (
+      <div style={props.divStyle} ref={r}>
         <Tree
-          data={treeToData(tree)}
+          data={treeToData(props.tree)}
           translate={t ?? { x: 0, y: 0 }}
           nodeSize={nodeSize}
           renderCustomNodeElement={rd3tProps =>
-            renderForeignObjectNode(rd3tProps, pos, foreignObjectProps)}
+            renderForeignObjectNode(rd3tProps, props.pos, foreignObjectProps)}
           orientation='vertical'
           pathFunc={'straight'} />
+      </div>
     )
 }
 
-export default function RenderDisplay(props: DisplayTreeProps) : JSX.Element {
-    const pos = props.pos
-    const [selectedLocs, setSelectedLocs] = React.useState<GoalsLocation[]>([]);
-    const locs = React.useMemo(() => ({
-        isSelected: (l : GoalsLocation) => selectedLocs.some(v => GoalsLocation.isEqual(v, l)),
-        setSelected: (l : GoalsLocation, act : any) => setSelectedLocs(ls => {
-            // We ensure that `selectedLocs` maintains its reference identity if the selection
-            // status of `l` didn't change.
-            const newLocs = ls.filter(v => !GoalsLocation.isEqual(v, l));
-            const wasSelected = newLocs.length !== ls.length;
-            const isSelected = typeof act === 'function' ? act(wasSelected) : act;
-            if (isSelected)
-                newLocs.push(l);
-            return wasSelected === isSelected ? ls : newLocs;
-        }),
-        subexprTemplate: { mvarId: '', loc: { target: '' }}
-    }), [selectedLocs]);
-    props.selectedLocations = selectedLocs;
-    React.useEffect(() => setSelectedLocs([]), [pos.uri, pos.line, pos.character]);
-    const r = React.useRef<HTMLDivElement>(null)
-    return (
-    <div
-      style={{
-        height: '400px',
-        display: 'inline-flex',
-        minWidth: '600px',
-        border: '1px solid rgba(100, 100, 100, 0.2)',
-        overflow: 'hidden', 
-        resize: 'both',
-        opacity: '0.9',
-      }}
-      ref={r}
-    >
-     <LocationsContext.Provider value = {locs}>
-        <RenderDisplayTree pos={pos} tree={props.tree} r={r} />
-     </LocationsContext.Provider>
-     <div>{selectedLocs.map(loc => {console.log(loc); return <p>Location selected</p>})}</div>
-    </div>)
-}
+// export default function RenderDisplay(props: DisplayTreeProps) : JSX.Element {
+//     const pos = props.pos
+//     const [selectedLocs, setSelectedLocs] = React.useState<GoalsLocation[]>([]);
+//     const locs = React.useMemo(() => ({
+//         isSelected: (l : GoalsLocation) => selectedLocs.some(v => GoalsLocation.isEqual(v, l)),
+//         setSelected: (l : GoalsLocation, act : any) => setSelectedLocs(ls => {
+//             // We ensure that `selectedLocs` maintains its reference identity if the selection
+//             // status of `l` didn't change.
+//             const newLocs = ls.filter(v => !GoalsLocation.isEqual(v, l));
+//             const wasSelected = newLocs.length !== ls.length;
+//             const isSelected = typeof act === 'function' ? act(wasSelected) : act;
+//             if (isSelected)
+//                 newLocs.push(l);
+//             return wasSelected === isSelected ? ls : newLocs;
+//         }),
+//         subexprTemplate: { mvarId: '', loc: { target: '' }}
+//     }), [selectedLocs]);
+//     props.selectedLocations = selectedLocs;
+//     React.useEffect(() => setSelectedLocs([]), [pos.uri, pos.line, pos.character]);
+//     const r = React.useRef<HTMLDivElement>(null)
+//     return (
+    
+//      <LocationsContext.Provider value={locs}>
+//         {RenderDisplayTree(props)}
+//      </LocationsContext.Provider>)
+// }
