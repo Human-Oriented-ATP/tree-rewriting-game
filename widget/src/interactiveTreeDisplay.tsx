fd3e7b9ce16eab1ec89906ab301e1c3f00452bcc
ART0 <18333981+0Art0@users.noreply.github.com>
2024-02-05 09:31:29 +0000

    Clear dead code from `interactiveTreeDisplay`

--- widget/src/interactiveTreeDisplay.tsx
+++ widget/src/interactiveTreeDisplay.tsx
@@ -73,30 +73,3 @@ export default function RenderDisplayTree(props : DisplayTreeProps): JSX.Element
       </div>
     )
 }
-
-// export default function RenderDisplay(props: DisplayTreeProps) : JSX.Element {
-//     const pos = props.pos
-//     const [selectedLocs, setSelectedLocs] = React.useState<GoalsLocation[]>([]);
-//     const locs = React.useMemo(() => ({
-//         isSelected: (l : GoalsLocation) => selectedLocs.some(v => GoalsLocation.isEqual(v, l)),
-//         setSelected: (l : GoalsLocation, act : any) => setSelectedLocs(ls => {
-//             // We ensure that `selectedLocs` maintains its reference identity if the selection
-//             // status of `l` didn't change.
-//             const newLocs = ls.filter(v => !GoalsLocation.isEqual(v, l));
-//             const wasSelected = newLocs.length !== ls.length;
-//             const isSelected = typeof act === 'function' ? act(wasSelected) : act;
-//             if (isSelected)
-//                 newLocs.push(l);
-//             return wasSelected === isSelected ? ls : newLocs;
-//         }),
-//         subexprTemplate: { mvarId: '', loc: { target: '' }}
-//     }), [selectedLocs]);
-//     props.selectedLocations = selectedLocs;
-//     React.useEffect(() => setSelectedLocs([]), [pos.uri, pos.line, pos.character]);
-//     const r = React.useRef<HTMLDivElement>(null)
-//     return (
-    
-//      <LocationsContext.Provider value={locs}>
-//         {RenderDisplayTree(props)}
-//      </LocationsContext.Provider>)
-// }
