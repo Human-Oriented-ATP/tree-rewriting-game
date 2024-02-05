8cbd302dc5da5fb14c0fe7ebae36eb5212752c4c
ART0 <18333981+0Art0@users.noreply.github.com>
2024-02-05 08:29:01 +0000

    Rewrite rule display

--- TreeRewritingGame/OrdinaryDisplay.lean
+++ TreeRewritingGame/OrdinaryDisplay.lean
@@ -1,17 +1,37 @@
 import TreeRewritingGame.DisplayTree
 import ProofWidgets 
 
-open Lean Meta Elab Server Tactic ProofWidgets 
+open Lean Meta Elab Server Tactic ProofWidgets Json Jsx
 
-structure TreeDisplay extends PanelWidgetProps where 
+structure TreeDisplayProps where
   tree : DisplayTree
+  divStyle : Json := json% {
+        height: "40%",
+        display: "inline-flex",
+        width: "100%",
+        border: "1px solid rgba(100, 100, 100, 0.2)",
+        overflow: "hidden", 
+        resize: "both",
+        opacity: "0.9"
+    }
 deriving RpcEncodable
 
 @[widget_module]
-def OrdinaryTreeDisplay : Component TreeDisplay where
+def TreeDisplay : Component TreeDisplayProps where
   javascript := include_str ".." / "build" / "js" / "interactiveTreeDisplay.js"
 
 syntax (name := tree_display) "with_tree_display" tacticSeq : tactic
+
+def displayRewriteRule (lhs : Expr) (rhs : Expr) : MetaM Html := do
+  let .some lhsTree ← lhs.toDisplayTreeAtRoot | throwError m!"Failed to display {lhs} as tree."
+  let .some rhsTree ← rhs.toDisplayTreeAtRoot | throwError m!"Failed to display {rhs} as tree."
+  return (
+    <div style={json% {width: "100%", overflow: "hidden", display: "inline-flex"}}>
+      <TreeDisplay tree={lhsTree} divStyle={json% {width:"50%", display:"inline-block"}} />
+      <span style={json% {alignItems: "center", fontSize: "50px"}}>→</span>
+      <TreeDisplay tree={rhsTree} divStyle={json% {width:"50%", display:"inline-block"}} />
+    </div>
+  )
 
 @[tactic tree_display]
 def treeDisplay : Tactic
@@ -19,10 +39,10 @@ def treeDisplay : Tactic
     let tgt ← withTransparency .instances do 
       reduceAll (← getMainTarget)
     let t ← (tgt.toDisplayTree).run .root 
-    savePanelWidgetInfo stx ``OrdinaryTreeDisplay do
+    savePanelWidgetInfo stx ``TreeDisplay do
       return json% { tree : $(← rpcEncode t) }
     evalTacticSeq tacs
-  | _ => throwUnsupportedSyntax
+  | _ => Elab.throwUnsupportedSyntax
 
 example : True := by 
   with_panel_widgets[GoalTypePanel]
